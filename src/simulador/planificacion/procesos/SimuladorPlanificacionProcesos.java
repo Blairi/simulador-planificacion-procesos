5b94f131ce41b85e9966c01469f84bad1343cdbc
Blairi <axel.avi.fer@gmail.com>
2024-03-11 08:45:08 -0600

    primer commit

---
+++ src/simulador/planificacion/procesos/SimuladorPlanificacionProcesos.java
@@ -0,0 +1,9 @@
+package simulador.planificacion.procesos;
+
+public class SimuladorPlanificacionProcesos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello mis chicos");
+    }
+    
+}

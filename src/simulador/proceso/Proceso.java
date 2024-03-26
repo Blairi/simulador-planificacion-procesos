3fe8528ba7c0327bbeb0d1ae7ac64332a111e139
Blairi <axel.avi.fer@gmail.com>
2024-03-26 11:59:16 -0600

    renombrando atributo

--- src/simulador/proceso/Proceso.java
+++ src/simulador/proceso/Proceso.java
@@ -6,17 +6,17 @@ public class Proceso {
     private int id; // Identificador del proceso
     private String nombre; // Nombre del proceso
     private int tamanio; // Tamanio del proceso
-    private int tiempoEjecucion; // Tiempo de ejecucion del proceso
+    private int tiempoServicio; // Tiempo de ejecucion del proceso
     private int tiempoLlegada; // Tiempo de llegada
 
     public Proceso() {
     }
 
-    public Proceso(int id, String nombre, int tamanio, int tiempoEjecucion, int tiempoLlegada) {
+    public Proceso(int id, String nombre, int tamanio, int tiempoServicio, int tiempoLlegada) {
         this.id = id;
         this.nombre = nombre;
         this.tamanio = tamanio;
-        this.tiempoEjecucion = tiempoEjecucion;
+        this.tiempoServicio = tiempoServicio;
         this.tiempoLlegada = tiempoLlegada;
     }
 
@@ -44,12 +44,12 @@ public class Proceso {
         this.tamanio = tamanio;
     }
 
-    public int getTiempoEjecucion() {
-        return tiempoEjecucion;
+    public int getTiempoServicio() {
+        return tiempoServicio;
     }
 
-    public void setTiempoEjecucion(int tiempoEjecucion) {
-        this.tiempoEjecucion = tiempoEjecucion;
+    public void setTiempoServicio(int tiempoServicio) {
+        this.tiempoServicio = tiempoServicio;
     }
 
     public int getTiempoLlegada() {
@@ -62,7 +62,7 @@ public class Proceso {
 
     @Override
     public String toString() {
-        return "Proceso{" + "id=" + id + ", nombre=" + nombre + ", tamanio=" + tamanio + ", tiempoEjecucion=" + tiempoEjecucion + ", tiempoLlegada=" + tiempoLlegada + '}';
+        return "Proceso{" + "id=" + id + ", nombre=" + nombre + ", tamanio=" + tamanio + ", tiempoServicio=" + tiempoServicio + ", tiempoLlegada=" + tiempoLlegada + '}';
     }
    
 }
